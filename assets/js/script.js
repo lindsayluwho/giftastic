4201df14d7ab46c60ecedd08adfd071a954c28bc
Lindsay Brown <lindsayluwho@gmail.com>
2017-12-19 17:25:26 -0500

    now fully functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,8 @@
 //create a 'topics' array of search queries for GIPHY API
 var topics = ["cats", "dogs", "goats", "llamas"];
 //get form input and add to 'topics' array
-//add buttons for each index of topics array, data value should be equal to the button text which is the index of the array, display in horizontal scroll nav container
-function renderButtons() {
 
-    // Deletes the topics prior to adding new topics
-    // (this is necessary otherwise you will have repeat buttons)
-    $("#topics").empty();
-    // Loops through the array of topics
-    for (var i = 0; i < topics.length; i++) {
-
-        // Then dynamicaly generates buttons for each topic in the array
-        var a = $("<button>");
-        // Adds a class of topic to our button
-        a.addClass("btn btn-lg btn-primary");
-        // Added a data-attribute
-        a.attr("data-name", topics[i]);
-        if (i == 0) {
-            a.attr("aria-selected", "true");
-        }
-        a.attr("id", "topic");
-        // Provided the initial button text
-        a.text(topics[i]);
-        // Added the button to the topics div
-        $("#topics").append(a);
-    }
-
-};
-
-renderButtons();
-
-//Handle events where the Add button is clicked
-$("#submit-animal").on("click", function(event) {
-    event.preventDefault();
-    var topic = $("#add-animal").val().trim();
-    topics.push(topic);
-    renderButtons();
-    $("input").val("");
-    console.log(topics);
-});
-
-//when a button is clicked, trigger AJAX call to GIPHY API based on search query from button's data value, return 10 gifs
-
-$(".btn-primary").click(function() {
-
+function displayGifs() {
     $("#gifs-display-here").empty();
     $("#topic").attr("aria-selected", "true");
     console.log($(this).attr("data-name"));
@@ -111,11 +70,52 @@ $(".btn-primary").click(function() {
                 $(this).attr("data-state", "still");
             }
 
-
-
+            renderButtons();
         });
-
-
-
     });
-});
+  };
+
+    //add buttons for each index of topics array, data value should be equal to the button text which is the index of the array, display in horizontal scroll nav container
+
+    function renderButtons() {
+
+        // Deletes the topics prior to adding new topics
+        // (this is necessary otherwise you will have repeat buttons)
+        $("#topics").empty();
+        // Loops through the array of topics
+        for (var i = 0; i < topics.length; i++) {
+
+            // Then dynamicaly generates buttons for each topic in the array
+            var a = $("<button>");
+            // Adds a class of topic to our button
+            a.addClass("btn btn-lg btn-primary");
+            // Added a data-attribute
+            a.attr("data-name", topics[i]);
+            if (i == 0) {
+                a.attr("aria-selected", "true");
+            }
+            a.attr("id", "topic");
+            // Provided the initial button text
+            a.text(topics[i]);
+            // Added the button to the topics div
+            $("#topics").append(a);
+        }
+
+    };
+
+    //Handle events where the Add button is clicked
+
+    $("#submit-animal").on("click", function(event) {
+        event.preventDefault();
+        var topic = $("#add-animal").val().trim();
+        topics.push(topic);
+        renderButtons();
+        $("input").val("");
+        console.log(topics);
+    });
+
+    //add click listener for animal buttons
+    $(document).on("click", ".btn-primary", displayGifs);
+
+    //call the renderButtons function to display the initial buttons
+    renderButtons();
